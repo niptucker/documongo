f58f9e3f8dd1c2e7ea4047fb94c5aabd83ef432c
Tikhon Tagunov <niptuckker@gmail.com>
2014-05-19 19:44:43 +0400

    minors

--- documongo/MongoObject/Document.php
+++ documongo/MongoObject/Document.php
@@ -41,7 +41,6 @@ class Document extends \documongo\MongoObject {
         }
 
         $isPermitted = $this->typeObject->hasPermission($userUuid, $action, $xpath, $lang);
-
         if ($this->exists()) {
 
             if ($isPermitted || is_null($isPermitted)) {
@@ -145,8 +144,8 @@ class Document extends \documongo\MongoObject {
     }
 
     static function find($mn, $prefix, $type) {
-        $elems = array();
 
+$elems = array();  
         $entries = $mn->selectDB($prefix . "data")->documents->find(array("type" => $type));
         foreach ($entries as $entry) {
             $elems[] = new self($mn, $prefix, $entry);
@@ -239,7 +238,7 @@ class Document extends \documongo\MongoObject {
 
                     if (isset($item["accumulative"]) && !$item["accumulative"]) {
                         $oldValues[$fieldI18nName] = $fieldI18nValue;
-                        $this->setField($fieldI18nName, "");
+                        $this->setField($fieldI18nName, 0.0);
                     }
                 }
                 $ok = $this->save();
@@ -387,7 +386,7 @@ class Document extends \documongo\MongoObject {
             if (isset($entry["versions"]) && is_array($entry["versions"])) {
                 $versions = $entry["versions"];
             }
-            // var_dump($versions);
+         //     var_dump($versions);
         }
 
         return $versions;
