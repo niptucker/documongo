051642980d2d7e633759ced678adc912cab574d9
Тихон Тагунов <tikhontagunov@herzen.spb.ru>
2015-03-11 19:57:05 +0300

    Вернул обратно тест создания и удаления документа

--- tests/MongoObject/DocumentTest.php
+++ tests/MongoObject/DocumentTest.php
@@ -1,53 +1,51 @@
 <?php
 
-// // require_once('PHPUnit/Autoload.php');
-// // require_once('../../documongo/Load.php');
+use \documongo\MongoObject\DocumentType;
+use \documongo\MongoObject\Document;
+use \MongoClient;
 
-// use \documongo\MongoObject\DocumentType;
-// use \documongo\MongoObject\Document;
+class DocumentTest extends PHPUnit_Framework_TestCase
+{
 
-// class DocumentTest extends PHPUnit_Framework_TestCase
-// {
+    /**
+     * @dataProvider providerForCreate
+     */
+    public function testCreateDelete($mn, $prefix, $typeObject, $uuid)
+    {
 
-//     /**
-//      * @dataProvider providerCreate
-//      */
-//     public function testCreateDelete($mn, $prefix, $typeObject, $uuid)
-//     {
+        $testDocument = Document::create($mn, $prefix, $typeObject, $uuid);
 
-//         $testDocument = Document::create($mn, $prefix, $typeObject, $uuid);
+        $this->assertEquals($testDocument->uuid, $uuid);
 
-//         $this->assertEquals($testDocument->uuid, $uuid);
+        $ok = $testDocument->save();
 
-//         $ok = $testDocument->save();
+        $this->assertEquals($ok, true);
 
-//         $this->assertEquals($ok, true);
-
-//         $this->assertEquals($testDocument->uuid, $uuid);
+        $this->assertEquals($testDocument->uuid, $uuid);
 
 
-//         $testDocument2 = Document::findByUuid($mn, $prefix, $uuid);
+        $testDocument2 = Document::findByUuid($mn, $prefix, $uuid);
 
-//         $this->assertEquals($testDocument2->uuid, $uuid);
+        $this->assertEquals($testDocument2->uuid, $uuid);
 
-//         $this->assertEquals($testDocument, $testDocument2);
+        $this->assertEquals($testDocument, $testDocument2);
 
-//         $deleted = $testDocument2->delete();
+        $deleted = $testDocument2->delete();
 
-//         $this->assertEquals($deleted, true);
-//     }
+        $this->assertEquals($deleted, true);
+    }
 
-//     public function providerCreate()
-//     {
-//         $mn = new \MongoClient();
-//         $prefix = "temp_test_";
+    public function providerForCreate()
+    {
+        $mn = new MongoClient();
+        $prefix = "temp_test_";
 
-//         $typeObject = DocumentType::findByType($mn, $prefix, "faculty");
+        $typeObject = DocumentType::findByType($mn, $prefix, "test_type");
 
-//         $uuid = "test-uuid";
+        $uuid = "test-uuid";
 
-//         return array(array($mn, $prefix, $typeObject, $uuid));
-//     }
+        return array(array($mn, $prefix, $typeObject, $uuid));
+    }
 
-// }
+}
 
